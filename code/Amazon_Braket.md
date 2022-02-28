159cfc8893c6ac9ddf6fd82e6bac9e03ae50e18b
Patrick Huber <22phuber@gmail.com>
2022-02-28 11:36:43 +0100

    Added Amazon Braket Plugin. Restructured README and script

---
+++ code/Amazon_Braket.md
@@ -0,0 +1,13 @@
+- [PennyLane Plugin](#pennylane-plugin)
+- [Amazon Braket](#amazon-braket)
+
+# PennyLane Plugin
+[PennyLane-Braket Plugin](https://amazon-braket-pennylane-plugin-python.readthedocs.io/en/latest/)
+
+# Amazon Braket
+[Amazon Braket](https://aws.amazon.com/braket/)
+
+Examples:
+- https://github.com/aws/amazon-braket-examples
+  - [Getting Started](https://github.com/aws/amazon-braket-examples/blob/main/examples/pennylane/0_Getting_started.ipynb)
+
--- code/pennylane_setup.sh
+++ code/pennylane_setup.sh
@@ -1,6 +1,31 @@
-#!/bin/bash
-echo "Setting up Pennylane"
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+
+printf "Local Python paths\n"
+printf 'python: %s\n' "$(which python)"
+printf 'pip: %s\npip3: %s\n' "$(which pip)" "$(which pip3)"
+
+printf "\n"
+
+printf "Install/Upgrade Pennylane ...\n"
 pip install pennylane --upgrade
-pip install pennylane-sf pennylane-qiskit pennylane-cirq pennylane-forest pennylane-qsharp
+
+printf "Install Pennylane Plugins ...\n"
+pip install \
+  pennylane-sf \
+  pennylane-qiskit \
+  amazon-braket-pennylane-plugin \
+  pennylane-cirq \
+  pennylane-forest \
+  pennylane-qsharp
+
+printf "Install Interfaces ...\n"
 pip install autograd "tensorflow>=1.13.2" jax jaxlib
-pip3 install torch torchvision torchaudio
+pip install torch torchvision torchaudio
+
+printf "Install Amazon Braket Python SDK ...\n"
+pip install amazon-braket-sdk
+
+printf "Install/Upgrade boto3 ...\n"
+pip install boto3 --upgrade
