bdc32431538be21be52497bd4f14e05356c237a2
Ricardo Simoes <ricardo_simoes_93@hotmail.com>
2022-03-17 19:48:05 +0100

    fixes array

--- code/mqo/shots_comparisons.sh
+++ code/mqo/shots_comparisons.sh
@@ -5,8 +5,7 @@ clear
 if ! [ -d runs/shots_comparison ]; then
     mkdir runs/shots_comparison
 fi
-#shots=( 1 10 25 50 75 100 150 200 350 500 1000 1500 2000)
-shots=( 1500 2000 )
+shots=( 1 10 25 50 75 100 150 200 350 500 1000 1500 2000 10000)
 for i in "${shots[@]}"
 do
     if ! [ -d runs/shots_comparison/shots"$i" ]; then
